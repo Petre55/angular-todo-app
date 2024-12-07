6e01ebfeced16f1157f21607f62b68378c0ef862
Veres <h379295@stud.u-szeged.hu>
2024-12-07 16:41:54 +0100

    a

---
+++ src/app/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVcB4OVfqTQT0g8ODGdnopMtpRgxwGs_E",
+    authDomain: "todolist-1a983.firebaseapp.com",
+    projectId: "todolist-1a983",
+    storageBucket: "todolist-1a983.firebasestorage.app",
+    messagingSenderId: "484825259277",
+    appId: "1:484825259277:web:d5a62ba34822cbf01ea444",
+    measurementId: "G-T2LFDFXJHS"
+  };
